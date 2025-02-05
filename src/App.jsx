e64ab4457855a059df6906aa3651291ee5762b3f
PauloAlves8039 <pj38alves@gmail.com>
2025-02-05 13:27:19 -0300

    feat: define renderização condicional

--- src/App.jsx
+++ src/App.jsx
@@ -91,18 +91,21 @@ function App() {
                 <h4>Jersey Shop</h4>
                 
                 {items.map(item =>
-                    <div key={item.id} className="product">
+                    <div key={item.id} className={`product ${item.isInBag ? "selected" : ""}`}>
                         <div className="photo">
                             <img src={"./img/" + item.photo} />
                         </div>
                         <div className="description">
                             <span className="name">{item.name}</span>
                             <span className="price">{item.price}</span>
-                            <div className="quantity-area">
-                                <button>-</button>
-                                <span className="quantity">{item.quantity}</span>
-                                <button>+</button>
-                            </div>
+                            {
+                                item.isInBag &&
+                                <div className="quantity-area">
+                                    <button>-</button>
+                                    <span className="quantity">{item.quantity}</span>
+                                    <button>+</button>
+                                </div>
+                            }
                         </div>
                     </div>
                 )}
